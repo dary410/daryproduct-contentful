8bc3a55e0096d831a00ccb931b532e8de7597579
Tejui Hsu <dhsu@dhsu-ltm1.internal.salesforce.com>
2020-01-10 18:21:01 -0800

    first commit

---
+++ src/components/hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Img from 'gatsby-image'
+import "./mystyles.scss"
+import styles from './hero.module.css'
+
+export default ({ data }) => (
+  <div className="hero is-primary is-medium">
+    <div className="hero-body">
+      <div className='container'>
+        <h1 className="title">Dary Hsu</h1>
+        <h2 className="subtitle">Dary</h2>
+      </div>
+    </div>
+  </div>
+)
